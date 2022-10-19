7ed8c9f7edda1609bc4f80d67f001d54124c755a
Maksim Lyanko <maksim.lyanko@bk.ru>
2022-10-19 14:20:40 +0300

    added validation in form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import Button from '../Button/Button'
-import { useTelegram } from '../../hooks/useTelegram'
-import { Link } from 'react-router-dom'
 
 const Header = () => {
-
-  const {user, onClose} = useTelegram()
-
   return (
     <header className='header'>
+      
     </header>
   )
 }
